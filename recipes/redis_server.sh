624fe58ba50a990cb997226e977f484ec5385482
Ricardo Perez <ricardo.perez@wearefriday.com>
2014-11-27 12:08:01 +0000

    removing vagrant files; change provision file

--- recipes/redis_server.sh
+++ recipes/redis_server.sh
@@ -1,1 +1,25 @@
 #!/bin/sh
+yum install make gcc wget -y
+
+cd /tmp
+wget http://download.redis.io/releases/redis-2.8.17.tar.gz
+tar xzf redis-2.8.17.tar.gz
+cd redis-2.8.17
+make
+make install
+
+cd ..
+wget https://raw.githubusercontent.com/ricardoperez/redis-template/master/redis-server -O /etc/init.d/redis-server
+sudo chmod u+x /etc/init.d/redis-server
+
+if [ ! -d '/etc/redis' ]; then
+  sudo mkdir /etc/redis/
+fi
+wget https://raw.githubusercontent.com/ricardoperez/redis-template/master/redis.conf -O /etc/redis/redis.conf
+
+if [ ! -d '/var/lib/redis' ]; then
+  sudo mkdir /var/lib/redis/
+fi
+
+sudo /etc/init.d/redis-server start
+cd ~
